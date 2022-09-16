81a1095c504692e3026e2b587445022e9901e732
imkrrish <imkrrish227@gmail.com>
2022-09-16 12:31:06 +0530

    file in alphabetical order

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let tableRaw = document.getElementById("tbody");
 inputTypeFile.addEventListener("change", function () {
 
   if (this.files.length > 0) {
-    for (var i = 1; i <= this.files.length; i++) {
+    let filesDetails = [];
+    for (var i = 0; i < this.files.length; i++) {
       let filename = this.files.item(i).name;
       let Name = filename.substring(0, filename.lastIndexOf(".")) || filename;
-      let size = "";
-
+      let size = ""
       function formatFileSize(bytes) {
         if (bytes == 0) {
           size = "0 Bytes";
@@ -23,13 +23,27 @@ inputTypeFile.addEventListener("change", function () {
       }
 
       formatFileSize(this.files.item(i).size);
-      let tooltiptext = "<p>name: " + Name + "<br>type: " + this.files.item(i).type + "<br>size: " + size + "</p>";
+
+      let efile = {
+        "Name": Name,
+        "Size": size,
+        "Type": this.files.item(i).type,
+      };
+      filesDetails.push(efile);
+    }
+
+    filesDetails.sort((a,b) => (a.Name > b.Name ? 1 : -1));
+
+    for (var i = 1; i <= filesDetails.length; i++) {
+      let n = i-1;
+      let Name = filesDetails[n].Name;
+      let tooltiptext = "<p>name: " + Name + "<br>type: " + filesDetails[n].Type + "<br>size: " + filesDetails[n].Size + "</p>";
 
       tableRaw.innerHTML +=
         "<tr>" +
           "<td data-lable='S.No'>" + i + "</td>" +
           "<td data-lable='File Name'>" + Name + "</td>" +
-          "<td data-lable='size'>" + size + "</td>" +
+          "<td data-lable='size'>" + filesDetails[n].Size + "</td>" +
           "<td data-lable='file info'>" +
             "<i class='fa-solid fa-circle-info tooltip'><span class='tooltiptext'>" + tooltiptext + "</span></i>" +
           "</td>" +
